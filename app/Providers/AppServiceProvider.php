a1e6c96b088d2235b6bbebf66edad45713aae53b
Leslie <Sche0171@hz.nl>
2020-06-10 09:35:23 +0200

    service sequrity update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(config('app.env') === 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+            \URL::forceScheme('https');
+
+
+        }
     }
 }
